ea9cf056b4746779d1789b6702dd723c1cbbfae7
Mauro Bonanno <maurobonanno6@gmail.com>
2023-09-24 10:57:31 -0300

    estructura y base h2 del proyecto

---
+++ src/main/java/mauro/bonanno/ejercicio/precios/PreciosApplication.java
@@ -0,0 +1,13 @@
+package mauro.bonanno.ejercicio.precios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreciosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mauro/bonanno/ejercicio/precios/PreciosApplicationTests.java
@@ -0,0 +1,13 @@
+package mauro.bonanno.ejercicio.precios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
